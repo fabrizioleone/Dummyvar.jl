a9423005767fce52e384fdef7187722b0beec5d8
Fabrizio Leone <faleone@ulb.be>
2022-07-28 12:39:57 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using Dummyvar
 using Test
 
 @testset "Dummyvar.jl" begin
-    @test Dummyvar.dummy_create(collect(1:2)) == [1.0 0.0; 0.0 1.0]
-    @test Dummyvar.dummy_create([1.0, 0.0]) == [1.0 0.0; 0.0 1.0]
-    @test Dummyvar.dummy_create(['a', 'b']) == [1.0 0.0; 0.0 1.0]
-    @test Dummyvar.dummy_create(['a', 4]) == [1.0 0.0; 0.0 1.0]
+    @test Dummyvar.dummycreate(collect(1:2)) == [1.0 0.0; 0.0 1.0]
+    @test Dummyvar.dummycreate([1.0, 0.0]) == [1.0 0.0; 0.0 1.0]
+    @test Dummyvar.dummycreate(['a', 'b']) == [1.0 0.0; 0.0 1.0]
+    @test Dummyvar.dummycreate(['a', 4]) == [1.0 0.0; 0.0 1.0]
 end
